442e6e793f9d07f227ffc80668e24977566a8e61
uzayri <uzayr001@gmail.com>
2024-02-24 17:22:44 +0200

    simple first part of code to check if file reads in correctly

--- src/GenericsKbArrayApp.java
+++ src/GenericsKbArrayApp.java
@@ -3,5 +3,18 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class GenericsKbArrayApp {
-    
+    public static void main(String[] args) {
+        try {
+            File myFile = new File("GenericsKB.txt");
+            Scanner myScanner = new Scanner(myFile);
+            while (myScanner.hasNextLine()) {
+                String statement = myScanner.nextLine();
+                System.out.println(statement);
+            }
+            myScanner.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+    }
 }
